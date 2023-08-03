47a279c716994f6334be85b9a16f769e4b690bf8
bobby <rlatnguscjsw@gmail.com>
2023-08-03 23:52:51 +0900

    Feat : 배열 타입
    - 배열 타입

---
+++ section2/src/chapter2.ts
@@ -0,0 +1,16 @@
+let numArr: number[] = [1, 2, 3];
+
+let strArr: string[] = ["Hello", "world"];
+
+let boolArr: Array<boolean> = [true, false, true];
+
+let multiArr: (string | number) [] = [1, "Hello"];
+
+let doubleArr: number[][] = [
+	[1, 2, 3],
+	[4, 5, 6]
+]
+
+let tup1: [number, number] = [1, 2];
+
+let tup2: [number, string, boolean] = [1, "Hello", true];
